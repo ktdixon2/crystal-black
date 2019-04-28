dab01f0c31f14a831f3fe1ad1989a36ac13cadba
Ken Dixon <KDixon@kens-air.attlocal.net>
2019-04-28 07:49:47 -0500

    connected to API. Still troubleshooting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 console.log("Hello from Crystal Black")
+
+// Retrieve location on the DOM//
+
+const container = document.querySelector("#container")
+    console.log("main display", container)
+
+//Fetch all currency data//
+fetch("https://api.coinlore.com/api/tickers", {
+        mode: 'no-cors'
+})
+    .then(currencies => currencies.json())
+        console.log("currencies", currencies)
+    .then(parsedCurrencies => {
+        const allCurrencies = data.name
+        allCurrencies.forEach(currency => {
+
+            
+        })
+    })            
