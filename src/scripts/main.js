f51f589b2d0935e9385a1106cc972899564dcdd3
Ken Dixon <KDixon@kens-air.attlocal.net>
2019-04-28 14:02:24 -0500

    styling and additional data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,47 +1,19 @@
-console.log("Hello from Crystal Black")
-
 // Retrieve location on the DOM//
 
 const container = document.querySelector("#mainDisplay")
-    console.log("main display", mainDisplay);
 
 //Fetch all currency data//
 fetch(" https://api.coinlore.com/api/tickers/?start=0&limit=10")
     .then(currencies => currencies.json())
     .then(parsedCurrencies => {
-        //console.log(parsedCurrencies);
         parsedCurrencies.data.forEach(currency => {
-            console.log("currency", currency);
             const coinHTML = `
                     <article class="visited">
                     <h3>${currency.name}</h3>
                     <p>USDT \$${currency.price_usd}</p>
-                    <p>BTC ${currency.price_btc}`;
+                    <p>BTC ${currency.price_btc}</p>
+                    <p>24hr Change ${currency.percent_change_24h}</p>`;
                     container.innerHTML += coinHTML
         });
             
         });
-
-
-
-
-// console.log("Hello from Crystal Black")
-
-// // Retrieve location on the DOM//
-
-// const mainDisplay = document.querySelector("#mainDisplay")
-//     console.log("main display", mainDisplay)
-
-// //Fetch all currency data//
-// fetch("https://api.coinlore.com/api/tickers/?start=0&limit=10")
-//     .then(currencies => currencies.json())
-//     .then(parsedCurrencies => {
-//         const allCurrencies = parsedCurrencies.data
-//         allCurrencies.forEach(currency => {
-//             console.log("allcurrencies", allCurrencies);
-//             fetch(`https://api.coinlore.com/api/ticker/?id=${allCurrencies.id}`)
-                    
-            
-//         });
-            
-//         });         
