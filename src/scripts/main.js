cbb33140fd5b5c55ac848b4535cec49165aac0c0
Ken Dixon <KDixon@Kens-Macbook-Air.local>
2019-04-29 09:01:09 -0500

    more info and styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,17 @@
 const container = document.querySelector("#mainDisplay")
 
 //Fetch all currency data//
-fetch(" https://api.coinlore.com/api/tickers/?start=0&limit=10")
+fetch("https://api.coinlore.com/api/tickers/?start=0&limit=10")
     .then(currencies => currencies.json())
     .then(parsedCurrencies => {
         parsedCurrencies.data.forEach(currency => {
             const coinHTML = `
-                    <article class="visited">
-                    <h3>${currency.name}</h3>
-                    <p>USDT \$${currency.price_usd}</p>
-                    <p>BTC ${currency.price_btc}</p>
-                    <p>24hr Change ${currency.percent_change_24h}</p>`;
+                    <article class="currencies">
+                    <h2>${currency.name}</h2>
+                    <h4>${currency.symbol}</h4>
+                    <p>\$${currency.price_usd}</p>
+                    <p>${currency.price_btc}</p>
+                    <p>24hr ${currency.percent_change_24h}</p>`;
                     container.innerHTML += coinHTML
         });
             
