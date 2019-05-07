b8bbdf9bd97072ada3bb1e4a76a3aee5e2ed3958
Ken Dixon <KDixon@Kens-Macbook-Air.local>
2019-05-07 13:31:36 -0500

    added buttons and removed additional exchange code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,26 +12,21 @@ fetch("https://api.coinlore.com/api/tickers/?start=0&limit=10")
             const coinHTML = `
                     <article class="currencies">
                     <h2>${currency.name}</h2>
-                    <h4>${currency.symbol}</h4>
+                    <h4>\(${currency.symbol}\)</h4>
                     <p>\$${currency.price_usd}</p>
                     <p>${currency.price_btc}</p>
-                    <p>24hr ${currency.percent_change_24h}</p>`;
+                    <p>24hr ${currency.percent_change_24h}</p>
+                    <button id="chart">Chart</button>
+                    <button id="buy">Buy</button>`
                     container.innerHTML += coinHTML
         });
-            
     });
+ 
 
-fetch("https://www.bitmex.com/api/v1/chat", {
-    "mode": "no-cors"
-})
-    .then(response => response.json())
-    .then(parsedChats => {
-        parsedChats.forEach(chat => {
-            console.log("chat", parsedChats)
-            
-        });
-    });   
+    
 
 //Nonce using Epoch time. 
 
 const ts = Math.round((new Date()).getTime() / 1000);
+
+
